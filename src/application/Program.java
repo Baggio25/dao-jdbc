f5a245adf716099c2433d21154a7c2c2f93c2738
Baggio25 <rodrigo.baggio.si@gmail.com>
2021-06-18 00:09:33 -0300

    Reutilizando a instanciacao'

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,8 +11,9 @@ public class Program {
 	public static void main(String[] args) {
 
 		SellerDao sellerDao  = DaoFactory.createSellerDao();
+		
 		Seller seller = sellerDao.findById(3);
 		
-		JOptionPane.showMessageDialog(null, seller);
+		JOptionPane.showMessageDialog(null, "Test 1: \n" + seller);
 	}
 }
